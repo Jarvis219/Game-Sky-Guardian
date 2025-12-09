30945a4f6359d7eb3eabd041b86f8c795b49746c
Jarvis Tran <taquang.hskx.2000@gmail.com>
2025-12-09 10:09:31 +0700

    feat: update build repo

--- index.tsx
+++ index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+function initApp() {
+  const rootElement = document.getElementById("root");
+  console.log(rootElement);
+  if (!rootElement) {
+    throw new Error("Could not find root element to mount to");
+  }
+
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initApp);
+} else {
+  initApp();
+}
